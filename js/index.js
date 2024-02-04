b56cb564e173b1d61ef00643aab1a5e614c9993f
Srinivas <srinivas365@proton.me>
2024-02-04 22:47:24 +0530

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,6 @@
+function toggleMenu() {
+    document.querySelector(".hamburger").classList.toggle("active");
+    document.querySelector(".nav").classList.toggle("active");
+    document.querySelector(".header").classList.toggle("active");
+    document.querySelector(".main").classList.toggle("hidden");
+}
